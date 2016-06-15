b4b40c86b242d75ea947a9167fed877d2d6c2b4b
Robert Rudduck <robert@rpowered.net>
2016-06-15 09:16:08 -0500

    Update naming convention for install script.

--- extensions/node.sh
+++ extensions/node.sh
@@ -2,15 +2,14 @@
 
 cloud_type="azure"
 location=$1 #this is the location of the seed, not necessarily of this node
-unique_string=$2
+seed_node_name=$2
 data_center_name=$3
 
-seed_node_dns_name="dc0vm0$unique_string.$location.cloudapp.azure.com"
+seed_node_dns_name="$seed_node_name.$location.cloudapp.azure.com"
 
 echo "Configuring nodes with the settings:"
 echo cloud_type $cloud_type
 echo location $location
-echo unique_string $unique_string
 echo data_center_name $data_center_name
 echo seed_node_dns_name $seed_node_dns_name
 
