848a88b84c81c392120d34f6ec1ad24f379d3b75
Robert Rudduck <robert@rpowered.net>
2016-06-15 12:23:50 -0500

    Update install script parameters for opscenter.

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -2,14 +2,13 @@
 
 cloud_type="azure"
 location=$1 #this is the location of the seed, not necessarily of this node
-unique_string=$2
+seed_node_name=$2
 
-seed_node_dns_name="dc0vm0$unique_string.$location.cloudapp.azure.com"
+seed_node_dns_name="$seed_node_name.$location.cloudapp.azure.com"
 
 echo "Configuring nodes with the settings:"
 echo cloud_type $cloud_type
 echo location $location
-echo unique_string $unique_string
 echo seed_node_dns_name $seed_node_dns_name
 
 wget https://github.com/DSPN/install-datastax/archive/1.0.zip
