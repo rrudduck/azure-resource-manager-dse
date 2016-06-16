b1daa9dc1bae24c906e70acb36bfce90174a015f
Robert Rudduck <robert@rpowered.net>
2016-06-16 10:19:42 -0500

    Switch to using short name for seed node.

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -4,7 +4,7 @@ cloud_type="azure"
 location=$1 #this is the location of the seed, not necessarily of this node
 seed_node_name=$2
 
-seed_node_dns_name="$seed_node_name.$location.cloudapp.azure.com"
+seed_node_dns_name="$seed_node_name"
 
 echo "Configuring nodes with the settings:"
 echo cloud_type $cloud_type
